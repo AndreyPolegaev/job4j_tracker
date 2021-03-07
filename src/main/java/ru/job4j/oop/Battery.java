3f2805f1967d7e96a281b50fcab3043f764ac9fc
AndreyPolegaev <79646307753@yandex.ru>
2021-03-07 20:58:08 +0300

    1.8. Состояние объекта [#466308]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+/**
+ *  public void exchange method() removes called object's value (load field) and add this value to another object.
+ */
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        another.load += this.load;
+        this.load = 0;
+    }
+
+    public void showInfo() {
+        System.out.println("Load is... " + this.load + " %");
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(20);
+        Battery second = new Battery(5);
+        first.showInfo();
+        second.showInfo();
+        first.exchange(second);
+        System.out.println();
+        first.showInfo();
+        second.showInfo();
+    }
+}
