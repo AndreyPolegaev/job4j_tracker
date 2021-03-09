f8dab7f198a665637955d17bd133996afd3736b4
AndreyPolegaev <79646307753@yandex.ru>
2021-03-09 17:41:22 +0300

    3. Реализация профессий в коде [#466320]

--- src/main/java/ru/job4j/oop/profession/Builder.java
+++ src/main/java/ru/job4j/oop/profession/Builder.java
@@ -3,6 +3,11 @@ package ru.job4j.oop.profession;
 public class Builder extends Engineer {
     private String skills;
 
+    public Builder(String name, String surname, String education, String birthday, String skills) {
+        super(name, surname, education, birthday);
+        this.skills = skills;
+    }
+
     public void info() {
         System.out.println("Hello Im a builder i can offer a next service" + this.skills);
     }
