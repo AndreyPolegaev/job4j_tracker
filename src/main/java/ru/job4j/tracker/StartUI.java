d8db4b8d6cce270d635a19d3caa979c4416409f8
AndreyPolegaev <79646307753@yandex.ru>
2021-03-12 17:46:59 +0300

    9. toString [#466330]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,21 +2,9 @@ package ru.job4j.tracker;
 
 public class StartUI {
     public static void main(String[] args) {
-        Tracker tracker = new Tracker();
-        Item first = new Item();
-        first.setName("First Item");
-        tracker.add(first);
-        Item rsl = tracker.findById(1);
-        System.out.println(rsl.getId() + " " + rsl.getName());
-
-        Item[] findAll = tracker.findAll();
-        for (Item temp : findAll) {
-            System.out.println(temp.getId() + " " + temp.getName());
-        }
-
-        Item[] findByName = tracker.findByName("First Item");
-        for (Item temp : findByName) {
-            System.out.println(temp.getId());
-        }
+        Item item = new Item();
+        item.setName("TestItem");
+        item.setId(1);
+        System.out.println(item);
     }
 }
