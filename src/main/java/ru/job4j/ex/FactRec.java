8c2d1c943b556c4fa6e290d4efbb486c586fc476
AndreyPolegaev <79646307753@yandex.ru>
2021-03-20 23:21:39 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#466275]

---
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -0,0 +1,15 @@
+package ru.job4j.ex;
+/* Без базового условия получим StackOverflowError.*/
+public class FactRec {
+    public static int calc(int n) {
+        if (n < 2) {
+            return 1;
+        } else {
+            return n * calc(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calc(5));
+    }
+}
