0aa380c45c6fd85f41597b4d0b3d7042ff145fec
AndreyPolegaev <79646307753@yandex.ru>
2021-04-03 19:47:26 +0300

    2. Идентичные тексты [#466244]

--- src/test/java/ru/job4j/collection/set/FullSearchTest.java
+++ src/test/java/ru/job4j/collection/set/FullSearchTest.java
@@ -20,5 +20,4 @@ public class FullSearchTest {
         Set<String> expected = new HashSet<>(Arrays.asList("1", "2"));
         assertThat(FullSearch.extractNumber(tasks), is(expected));
     }
-
 }
