6f021a2a0d5df6fc2eac51937320137eb277f3fa
AndreyPolegaev <79646307753@yandex.ru>
2021-04-03 22:51:59 +0300

    1. Паспорт и жители [#466239]

---
+++ src/test/java/ru/job4j/collection/Map/passport/PassportOfficeTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection.Map.passport;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void whenCitizenIsAlreadyPresents() {
+        Citizen citizen = new Citizen("234fff232", "Silvester Stallone");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.add(new Citizen("234fff232", "Silvester Stallone")), is(false));
+    }
+}
