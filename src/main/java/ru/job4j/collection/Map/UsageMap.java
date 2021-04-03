598cd876c1f088f7ae5d70ad2a8e7896dc0641f7
AndreyPolegaev <79646307753@yandex.ru>
2021-04-03 21:54:45 +0300

    0. Map, HashMap [#466238]

---
+++ src/main/java/ru/job4j/collection/Map/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection.Map;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("avpolegaev@gmail.com", "Andrey Polegaev");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
