736f944caf033002e739709e4056129bb10e2178
AndreyPolegaev <79646307753@yandex.ru>
2021-04-04 23:30:52 +0300

    3. Банковские переводы [#466240]

--- src/main/java/ru/job4j/collection/Map/bank/BankService.java
+++ src/main/java/ru/job4j/collection/Map/bank/BankService.java
@@ -9,40 +9,38 @@ public class BankService {
     private Map<User, List<Account>> users = new HashMap<>(); /*Это поле содержит всех пользователей системы с привязанными к ним счетами.*/
 
     public void addUser(User user) {  /*Добавить пользователя в систему, если такого еще нет */
-        for (User temp : users.keySet()) {
-             if (!temp.getPassport().equals(user.getPassport())) {
-                 users.put(user, new ArrayList<Account>());
-             }
-        }
+        users.putIfAbsent(user, new ArrayList<Account>());
     }
 
     public void addAccount(String passport, Account account) {  /* Должен добавить новый счет к пользователю, найти по паспорту */
         User user = findByPassport(passport);
-        List<Account> listAccount = users.get(user);
-        for (Account temp : listAccount) {
-            if (!temp.equals(account)) {
-                listAccount.add(account);
-            }
+        List<Account> listAccount = users.get(user); /* получить список его счетов */
+        if (!listAccount.contains(account)) {
+            listAccount.add(account);
         }
     }
 
-    public User findByPassport(String passport) {
+    public User findByPassport(String passport) { /* поиск юзера по паспорту */
         User user = null;
         for (User temp : users.keySet()) {
             if (temp.getPassport().equals(passport)) {
                 user = temp;
+                break;
             }
         }
         return user;
     }
 
-    public Account findByRequisite(String passport, String requisite) {
+    public Account findByRequisite(String passport, String requisite) { /* метод ищет счет пользователя по реквизитам */
         Account account = null;
         User user = findByPassport(passport);
-        List<Account> list = users.get(user);
-        for (Account temp : list) {
-            if (temp.getRequisite().equals(requisite)) {
-                account = temp;
+        List<Account> list = users.get(user); /* получить список его счетов */
+        if (user != null && !list.isEmpty()) {
+            for (Account temp : list) {
+                if (temp.getRequisite().equals(requisite)) {
+                    account = temp;
+                    break;
+                }
             }
         }
         return account;
@@ -51,6 +49,12 @@ public class BankService {
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount != null && srcAccount.getBalance() >= amount && destAccount != null) {
+            destAccount.setBalance(destAccount.getBalance() + amount);
+            rsl = true;
+        }
         return rsl;
     }
 }
