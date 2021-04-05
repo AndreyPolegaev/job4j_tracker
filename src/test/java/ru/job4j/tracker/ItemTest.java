4dc1c8a042b1e9b5886a70180d0f8b315cd8858a
AndreyPolegaev <79646307753@yandex.ru>
2021-04-05 17:04:32 +0300

    0. Сортировка [#466228]

--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -2,10 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
@@ -15,34 +12,32 @@ public class ItemTest {
     @Test
     public void whenNaturalOrderById() {
         Item item1 = new Item("First");
-        item1.setId(5);
         Item item2 = new Item("Second");
-        item2.setId(2);
         Item item3 = new Item("Third");
+        item1.setId(5);
+        item2.setId(2);
         item3.setId(4);
         List<Item> list = Arrays.asList(item1, item2, item3);
         Collections.sort(list);
-        List<Integer> expected = new ArrayList<>();
-        for (Item temp : list) {
-            expected.add(temp.getId());
-        }
-        assertThat(expected, is(Arrays.asList(2, 4, 5)));
+        List<Item> expected = Arrays.asList(
+                item2, item3, item1
+        );
+        assertThat(expected, is(list));
     }
 
     @Test
     public void whenReverseOrderById() {
         Item item1 = new Item("First");
-        item1.setId(5);
         Item item2 = new Item("Second");
-        item2.setId(2);
         Item item3 = new Item("Third");
+        item1.setId(5);
+        item2.setId(2);
         item3.setId(4);
         List<Item> list = Arrays.asList(item1, item2, item3);
         Collections.sort(list, new SortDownById());
-        List<Integer> expected = new ArrayList<>();
-        for (Item temp : list) {
-            expected.add(temp.getId());
-        }
-        assertThat(expected, is(Arrays.asList(5, 4, 2)));
+        List<Item> expected = Arrays.asList(
+                item1, item3, item2
+        );
+        assertThat(expected, is(list));
     }
 }
