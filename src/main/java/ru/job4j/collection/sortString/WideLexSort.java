eb426c1385235a661c6fbe8e4d515832f67dbcb5
AndreyPolegaev <79646307753@yandex.ru>
2021-04-06 22:13:11 +0300

    4. Сортировка номера [#466225]

---
+++ src/main/java/ru/job4j/collection/sortString/WideLexSort.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection.sortString;
+
+import java.util.Comparator;
+
+public class WideLexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        int rsl = 0;
+        String[] dataLeft = left.split(". ");
+        String[] dataRight = right.split(". ");
+        int steps = Math.min(dataLeft[0].length(), dataRight[0].length());
+        String s1 = dataLeft[0];
+        String s2 = dataRight[0];
+        for (int i = 0; i < steps; i++) {
+            rsl = Integer.compare(s1.charAt(i), s2.charAt(i));
+            if (rsl != 0) {
+                break;
+            }
+        }
+        return rsl;
+    }
+}
