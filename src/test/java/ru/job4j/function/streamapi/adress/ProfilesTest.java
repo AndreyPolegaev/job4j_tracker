301f4ad5bdc5e374d3d87e5a042633141ab1e323
AndreyPolegaev <79646307753@yandex.ru>
2021-04-26 22:36:28 +0300

    3. Уникальность элементов и сортировка. [#466065]

--- src/test/java/ru/job4j/function/streamapi/adress/ProfilesTest.java
+++ src/test/java/ru/job4j/function/streamapi/adress/ProfilesTest.java
@@ -20,8 +20,29 @@ public class ProfilesTest {
         );
         List<Address> rsl = new Profiles().collect(list);
         List<Address> expected = List.of(
-                new Address("Moscow", "Street1", 1, 10),
                 new Address("Briansk", "Street2", 2, 16),
+                new Address("Moscow", "Street1", 1, 10),
+                new Address("Tomsk", "Street3", 3, 37)
+        );
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenWasDeleteDuplicate() {
+        Profile firstClient = new Profile(new Address("Moscow", "Street1", 1, 10));
+        Profile secondClient = new Profile(new Address("Briansk", "Street2", 2, 16));
+        Profile thirdClient = new Profile(new Address("Tomsk", "Street3", 3, 37));
+        Profile fourthClient = new Profile(new Address("Tomsk", "Street3", 3, 37));
+        List<Profile> list = List.of(
+                firstClient,
+                secondClient,
+                thirdClient,
+                fourthClient
+        );
+        List<Address> rsl = new Profiles().collect(list);
+        List<Address> expected = List.of(
+                new Address("Briansk", "Street2", 2, 16),
+                new Address("Moscow", "Street1", 1, 10),
                 new Address("Tomsk", "Street3", 3, 37)
         );
         assertThat(rsl, is(expected));
