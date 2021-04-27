dcd3def68c46a37f17dfa7a22d6d631d448470e7
AndreyPolegaev <79646307753@yandex.ru>
2021-04-27 12:48:36 +0300

    4. Преобразование List в Map. [#466066]

--- src/main/java/ru/job4j/function/streamapi/studenttomap/ExecuteStident.java
+++ src/main/java/ru/job4j/function/streamapi/studenttomap/ExecuteStident.java
@@ -1,6 +1,5 @@
 package ru.job4j.function.streamapi.studenttomap;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -8,7 +7,9 @@ import java.util.stream.Collectors;
 public class ExecuteStident {
     public Map<String, Student> execute(List<Student> students) {
         return students.stream()
-                .distinct()
-                .collect(Collectors.toMap(e -> e.getSurname(), e -> e));
+                .collect(Collectors.toMap(
+                        Student::getSurname,
+                        e -> e,
+                        (oldValue, newValue) -> newValue));
     }
 }
