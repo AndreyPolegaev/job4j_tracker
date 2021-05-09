831b4408fd9c9d24d12f56a8f71a52dc0c288e12
AndreyPolegaev <79646307753@yandex.ru>
2021-05-10 00:28:18 +0300

    1. Экзамен. Lambda, Stream API, JDK Features. [#466081]

--- src/main/java/ru/job4j/function/streameasy/EasyStream.java
+++ src/main/java/ru/job4j/function/streameasy/EasyStream.java
@@ -10,33 +10,30 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class EasyStream {
 
-    private final Stream<Integer> input;
-    private Function<Integer, Integer> map = x -> x;
-    private Predicate<Integer> filter = f -> true;
+    private List<Integer> list;
 
-    public EasyStream(Stream<Integer> input) {
-        this.input = input;
+    private EasyStream(List<Integer> list) {
+        this.list = list;
     }
 
     public static EasyStream of(List<Integer> source) {
-        return new EasyStream(source.stream());
+        return new EasyStream(source);
     }
 
     public EasyStream map(Function<Integer, Integer> fun) {
-        this.map = fun;
+        list = list.stream().map(fun).collect(Collectors.toList());
         return this;
     }
 
     public EasyStream filter(Predicate<Integer> fun) {
-        this.filter = fun;
+        list = list.stream().filter(fun).collect(Collectors.toList());
         return this;
     }
 
     public List<Integer> collect() {
-       return input.filter(filter).map(map).collect(Collectors.toList());
+        return List.copyOf(list);
     }
 }
